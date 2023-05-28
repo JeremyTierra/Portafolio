ecccec6809014f383183ef684bf18a2084fc74a1
JeremyTierra <123839410+JeremyTierra@users.noreply.github.com>
2023-05-28 14:22:08 -0500

    Vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
-
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 import Projects from './pages/Proyects';
 import Contact from "./pages/Contact"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
+
   );
 }
 
