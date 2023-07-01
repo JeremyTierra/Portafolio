d5f0f6745e2648bb2ad6bb8d52282200e5740763
JeremyTierra <123839410+JeremyTierra@users.noreply.github.com>
2023-07-01 12:03:49 -0500

    edit error Scroll my skills

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 
 function Button({ text }) {
+  const handleClick = () => {
+    const element = document.getElementById('MySkills');
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-    <a href="#MySkills">
-      <button className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
-        <div className="absolute inset-0 w-3 bg-teal-600 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-        <span className="relative text-black group-hover:text-white">{text}</span>
-      </button>
-    </a>
+
+    <button
+      className="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow"
+      onClick={handleClick}
+    >
+      <div className="absolute inset-0 w-3 bg-teal-600 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+      <span className="relative text-black group-hover:text-white">{text}</span>
+    </button>
+
   );
 }
 
