fe80cd5ee65b110ab52d5e387aaae17c827d7e28
JeremyTierra <123839410+JeremyTierra@users.noreply.github.com>
2023-06-30 20:06:21 -0500

    create hashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 import './App.css';
 import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
@@ -8,14 +8,15 @@ import Contact from "./pages/Contact"
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
+      <HashRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+      </HashRouter>,
+
       <Analytics />
     </>
 
